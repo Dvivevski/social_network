20b83ccca9f15425a0484cbc5a35901983a4a716
Vitalii Danylevskyi <vitalii.danylevskyi@gmail.com>
2022-08-17 17:01:40 +0300

    init_proj

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+	var elems = document.querySelectorAll('.sidenav');
+	var instances = M.Sidenav.init(elems, {});
+	var elems1 = document.querySelectorAll('.carousel');
+	var instances1 = M.Carousel.init(elems1, {});
+
+
+});
